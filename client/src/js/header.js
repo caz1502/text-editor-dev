a8b6b1b58495870e96c407adb2bbb6e459278fc9
Carol Shiers <carol.shiers@gmail.com>
2022-02-03 11:32:32 +1000

    updating

---
+++ client/src/js/header.js
@@ -0,0 +1,14 @@
+// complete
+
+const header = `
+/*
+       _____  ____________
+      / /   |/_  __/ ____/
+ __  / / /| | / / / __/   
+/ /_/ / ___ |/ / / /___   
+ ____/_/  |_/_/ /_____/   
+ 
+JUST ANOTHER TEXT EDITOR
+*/                          
+`;
+export { header };
